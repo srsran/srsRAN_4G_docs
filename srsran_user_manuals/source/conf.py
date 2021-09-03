9bf527da3939dc37e79200043316e4f934981d4e
Brendan <bmcaulif@tcd.ie>
2021-09-03 16:36:25 +0100

    Changes to conf.py to remove blank pages from PDF render. Other small edits to improve quality of render (issue 25)

--- srsran_user_manuals/source/conf.py
+++ srsran_user_manuals/source/conf.py
@@ -213,10 +213,10 @@ htmlhelp_basename = 'srsRANdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+'pointsize': '11pt',
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
@@ -242,7 +242,7 @@ latex_documents = [
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+latex_use_parts = False
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
